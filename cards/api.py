b25be71110a9eed397a87e33695c3adacfc3e5af
Artem Klushin/bin/python <arklual1@gmail.com>
2024-07-03 12:24:06 +0300

    add favorite

--- cards/api.py
+++ cards/api.py
@@ -1,22 +1,26 @@
 from datetime import datetime
 
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from ninja import Router, Schema, Field
 from django.contrib import auth
 from django.db import IntegrityError
 import jwt
 import os
 from parser_wb import parse
+from profiles.models import Profile
+from web2_backend.settings import SECRET_KEY
 
-from .models import Card
-from .schemas import CreateCard, Error
+from .models import Card, Favorite
+from .schemas import CreateCard, AddFavor, Error, CardSchema
+from authtoken import AuthBearer
 
 router = Router()
 
-@router.post('/create_card', response={201: CreateCard, 409: Error, 400: Error})
+@router.post('/create_card', response={201: CardSchema, 409: Error, 400: Error}, auth=AuthBearer)
 def create_card(request, create_card: CreateCard):
     data_from_wb = parse(create_card.target_url)
-    Card.objects.create(
+    c = Card.objects.create(
         name=data_from_wb['name'],
         url=create_card.target_url,
         image=data_from_wb['img'],
@@ -24,3 +28,17 @@ def create_card(request, create_card: CreateCard):
         category=create_card.category,
         shutdown_time=create_card.shutdown_time
     )
+    c.save()
+    return (201, c)
+
+@router.post('/add_favorite', response={201: AddFavor, 409: Error, 400: Error}, auth=AuthBearer)
+def create_card(request, addfavor: AddFavor):
+    card = get_object_or_404(Card, id=addfavor.card_id)
+    payload = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
+    user_id = payload['user_id']
+    user = get_object_or_404(Profile, id=user_id)
+    Favorite.objects.create(
+        card = card,
+        user = user
+    )
+    return (201, {'status': 'ok'})
--- cards/schemas.py
+++ cards/schemas.py
@@ -7,5 +7,16 @@ class CreateCard(Schema):
     category: str = Field(..., min_length=1, required=True)
     shutdown_time: str = Field(..., min_length=1, required=True)
 
+class AddFavor(Schema):
+    card_id: int = Field(..., required=True)
+
 class Error(Schema):
     details: str
+
+class CardSchema(Schema):
+    id: int
+    name: str
+    category: str
+    price: int
+    url: str
+    img: str
