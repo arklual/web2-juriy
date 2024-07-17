71b69155ce1a6c4a72c4502aafc77c1423b063f9
Artem Klushin/bin/python <arklual1@gmail.com>
2024-07-17 19:52:00 +0300

    add email config

--- web2_backend/settings.py
+++ web2_backend/settings.py
@@ -124,3 +124,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.beget.com'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'admin@idealpick.ru'
+EMAIL_HOST_PASSWORD = 'qF8-5Aq-w2c-T3Y'
+
+EMAIL_SERVER = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
