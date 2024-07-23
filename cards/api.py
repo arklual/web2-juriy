b7556cd5682bdf82fe31e26d8317a79d4bc99705
Артем Сиднев <artemsidnev@MacBook-Pro-Artem.local>
2024-07-23 13:33:39 +0300

    search

--- cards/api.py
+++ cards/api.py
@@ -10,7 +10,7 @@ import os
 from parser_wb import parse
 from profiles.models import Profile
 from web2_backend.settings import SECRET_KEY
-
+from fuzzywuzzy import fuzz
 from .models import Card, Favorite
 from .schemas import CreateCard, AddFavor, Error, CardSchema, StatusOK, UpdateCardSchema
 from authtoken import AuthBearer
@@ -124,3 +124,44 @@ def get_cards(request, start:int, count:int, sort:str,
         cards = cards.order_by('-price')# TODO: change sort
     cards = cards[start:(start+count)]
     return (200, cards)
+
+
+@router.get('/search', response={200: List[CardSchema], 409: Error, 400: Error})
+def search_cards(request, req: str, start:int, count:int, sort:str, 
+              time_start: Optional[str] = None, time_finish: Optional[str] = None, 
+              price_floor: Optional[int] = None, price_top: Optional[int] = None,
+              category: Optional[str] = None):
+    if sort not in ['recent', 'oldest', 'price_upscending', 'price_descending']:
+        return (400, {'details': 'sort parameter is not correct!'})
+    if time_start is None:
+        time_start = '01-01-2000'
+    if time_finish is None:
+        time_finish = '01-01-2100'
+    if price_floor is None:
+        price_floor = 0
+    if price_top is None:
+        price_top = 10000000000
+    time_start = datetime.strptime(time_start, '%m-%d-%Y').date()
+    time_finish = datetime.strptime(time_finish, '%m-%d-%Y').date()
+    cards = Card.objects.filter(price__gte=price_floor, price__lte=price_top).order_by('price')
+    # TODO: add filter by date
+    if category is not None:
+        category = get_object_or_404(Category, title=category)
+        cards = cards.filter(category=category)
+    if sort == 'price_upscending':
+        cards = cards.order_by('price')
+    elif sort == 'price_descending':
+        cards = cards.order_by('-price')
+    elif sort == 'recent':
+        cards = cards.order_by('-price')# TODO: change sort
+    elif sort == 'oldest':
+        cards = cards.order_by('-price')# TODO: change sort
+    cards = cards[start:(start+count)]
+    answer_cards = []
+    for card in cards:
+        ratio = fuzz.WRatio(req, card.name)
+        if ratio > 70:
+            answer_cards.append(card)
+    return (200, answer_cards)
+
+
